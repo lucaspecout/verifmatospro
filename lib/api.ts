37f7be1883ce7cc89e016ece613817206953ae1e
Lucas Pecout <100285952+lucaspecout@users.noreply.github.com>
2026-01-16 09:27:43 +0100

    Delete lib directory

--- lib/api.ts
+++
@@ -1,9 +0,0 @@
-import { NextResponse } from 'next/server';
-import { z } from 'zod';
-
-export const handleZodError = (error: unknown) => {
-  if (error instanceof z.ZodError) {
-    return NextResponse.json({ message: 'Validation error', issues: error.issues }, { status: 400 });
-  }
-  return NextResponse.json({ message: 'Erreur serveur' }, { status: 500 });
-};
